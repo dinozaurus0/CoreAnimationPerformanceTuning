c11f9a03c5c6d961c217e18e9a94a6747279645b
Vlad Grigore Sima <vladgrigoresima10@gmail.com>
2026-07-06 16:40:53 +0300

    Added shape layer for ring + its animation.

--- CoreAnimationPerformanceTuning/ViewController.swift
+++ CoreAnimationPerformanceTuning/ViewController.swift
@@ -19,13 +19,22 @@ final class ViewController: UIViewController {
     
     private func setupLayers() {
         let gradientLayer = gradientLayer()
-        let gradientAnimation =
-//        DispatchQueue.global().sync {
-            gradientColorsAnimation(using: gradientLayer.colors)
-//        }
+        
+        let (gradientAnimation, ringAnimation) =
+        DispatchQueue.global().sync {
+            (
+                gradientColorsAnimation(using: gradientLayer.colors),
+                ringLayerAnimation()
+            )
+        }
         
         gradientLayer.add(gradientAnimation, forKey: "gradientColorShift")
         view.layer.addSublayer(gradientLayer)
+        
+        let ringLayer = ringLayer()
+        
+        gradientLayer.addSublayer(ringLayer)
+        ringLayer.add(ringAnimation, forKey: "ringDrawAndSpin")
     }
     
     private func gradientLayer() -> CAGradientLayer {
@@ -65,6 +74,51 @@ final class ViewController: UIViewController {
         colorAnimation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
         
         return colorAnimation
+    }
+    
+    private func ringLayer() -> CAShapeLayer {
+        let ringLayer = CAShapeLayer()
+        
+        let ringSize: CGFloat = 200
+        ringLayer.bounds = CGRect(
+            x: 0,
+            y: 0,
+            width: ringSize,
+            height: ringSize
+        )
+        ringLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
+        
+        ringLayer.path = UIBezierPath(
+            ovalIn: CGRect(x: 0, y: 0, width: ringSize, height: ringSize).insetBy(dx: 3, dy: 3)
+        ).cgPath
+        
+        ringLayer.fillColor = UIColor.clear.cgColor
+        ringLayer.strokeColor = UIColor.white.cgColor
+        ringLayer.lineWidth = 6
+        ringLayer.lineCap = .round
+        
+        return ringLayer
+    }
+    
+    private func ringLayerAnimation() -> CAAnimationGroup {
+        let stroke = CABasicAnimation(keyPath: "strokeEnd")
+        stroke.fromValue = 0
+        stroke.toValue = 1
+        stroke.duration = 2
+        stroke.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+
+        let rotation = CABasicAnimation(keyPath: "transform.rotation.z")
+        rotation.fromValue = 0
+        rotation.toValue = CGFloat.pi * 2
+        rotation.duration = 8
+        rotation.repeatCount = .infinity
+
+        let group = CAAnimationGroup()
+        group.animations = [stroke, rotation]
+        group.duration = 8
+        group.repeatCount = .infinity
+        
+        return group
     }
     
 }
