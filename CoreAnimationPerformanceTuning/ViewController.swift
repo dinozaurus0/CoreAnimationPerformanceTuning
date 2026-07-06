d44f8abac54b8da190ba5095cca5c60d4a8ba2df
Vlad Grigore Sima <vladgrigoresima10@gmail.com>
2026-07-06 16:17:47 +0300

    Added the gradient layer.
    - contains only the properties setup, no animations.

--- CoreAnimationPerformanceTuning/ViewController.swift
+++ CoreAnimationPerformanceTuning/ViewController.swift
@@ -1,19 +1,37 @@
-//
-//  ViewController.swift
-//  CoreAnimationPerformanceTuning
-//
-//  Created by Vlad Grigore Sima on 06/07/2026.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    private var gradientLayer = CAGradientLayer()
+    private var bounds: CGRect = .zero
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        bounds = view.bounds
+        setupLayers()
     }
-
-
+    
+    private func setupLayers() {
+        self.setupGradientLayer()
+    }
+    
+    private func setupGradientLayer() {
+        gradientLayer.bounds = bounds
+        gradientLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
+        
+        gradientLayer.colors = [
+            UIColor.systemPurple.cgColor,
+            UIColor.systemBlue.cgColor,
+            UIColor.systemTeal.cgColor
+        ]
+        
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        
+        DispatchQueue.main.async {
+            self.view.layer.addSublayer(self.gradientLayer)
+        }
+    }
+    
 }
-
