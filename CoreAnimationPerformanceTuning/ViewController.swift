66c61adad455aa8d4225a0ec2655b9faf75bd67d
Vlad Grigore Sima <vladgrigoresima10@gmail.com>
2026-07-06 15:53:11 +0300

    Initial Commit

---
+++ CoreAnimationPerformanceTuning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreAnimationPerformanceTuning
+//
+//  Created by Vlad Grigore Sima on 06/07/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
